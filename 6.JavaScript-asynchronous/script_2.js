d80cf11a73fd596d3956efc36eb45eebf89f321e
Rafal Prokopiak <rafal@prokopiak.pl>
2021-11-26 14:47:21 +0100

    comments to describe my solution

--- 6.JavaScript-asynchronous/script_2.js
+++ 6.JavaScript-asynchronous/script_2.js
@@ -3,9 +3,13 @@
 class ReversePromise extends Promise {
     constructor(resolve, reject) {
         super(resolve, reject);
+        // assigning new method then
         this.then = this.then.bind(this);
+        // calls after first then call
         this.accumulator = this.accumulator.bind(this);
+        // list of all then inputs
         this.callList = [];
+        // calling checker
         this.beenActivated = false;
     }
 

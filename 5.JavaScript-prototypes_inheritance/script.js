96ecf450f6a4e9307810626bd526fd1e530f6f61
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2021-11-30 10:23:15 +0100

    Prototypes homework (#6)
    * working solution for the prototypes homework
    
    * refactore myNew function

---
+++ 5.JavaScript-prototypes_inheritance/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function myNew(constructorFunc, ...args) {
+    const tempItem = {};
+    Object.setPrototypeOf(tempItem, constructorFunc.prototype);
+    constructorFunc.apply(tempItem, args);
+    return tempItem;
+}
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.introduce = function () {
+    return "My name is " + this.name + " and I am " + this.age;
+};
+
+// var john = new Person("John", 30);
+var john = myNew(Person, "John", 30);
+// var jack = new Person("Jack", 40);
+var jack = myNew(Person, "Jack", 40);
+
+console.log(john.introduce()); //	My	name	is	John	and	I	am	30
+console.log(jack.introduce()); //	My	name	is	Jack	and	I	am	4
